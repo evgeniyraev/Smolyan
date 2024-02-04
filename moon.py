ac739198070d62e97c7fb844fee0ec8ed5613b0e
Evgeniy Raev <evgeniy.raev@zerogravityroom.com>
2024-02-04 14:32:53 +0000

    the curator can easily switch between languages

--- moon.py
+++ moon.py
@@ -3,13 +3,25 @@
 from time import sleep
 import pygame
 import keyboard
+import threading
+
+def set_audio_output():
+    pygame.mixer.init()
 
 def play_sound(file_path):
-    pygame.mixer.init()
     pygame.mixer.music.load(file_path)
     pygame.mixer.music.play()
 
+def stop_sound():
+    pygame.mixer.music.stop()
+
+def play_sound_thread(file_path):
+    thread = threading.Thread(target=play_sound, args=(file_path,))
+    thread.start()
+
 def main():
+    pygame.mixer.init()
+
     sound_file_bg = "/home/moon/service/moon_bg.mp3" 
     sound_file_en = "/home/moon/service/moon_en.mp3"
 
@@ -17,12 +29,16 @@ def main():
         try:
             if keyboard.is_pressed('Page_Down'):
                 print("starting bg, page down")
-                play_sound(sound_file_bg)
-                keyboard.wait('Page_Down')  # Wait for the key release to avoid continuous triggering
-            if keyboard.is_pressed('Page_Up'):
+                stop_sound()
+                play_sound_thread(sound_file_bg)
+                sleep(0.1)
+
+            elif keyboard.is_pressed('Page_Up'):
                 print("starting en, page up")
-                play_sound(sound_file_en)
-                keyboard.wait('Page_Up')  # Wait for the key release to avoid continuous triggering
+                stop_sound()
+                play_sound_thread(sound_file_en)
+                sleep(0.1)
+
         except KeyboardInterrupt:
             break
 
